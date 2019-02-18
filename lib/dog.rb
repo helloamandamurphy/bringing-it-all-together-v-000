bdb465bce3c167ee4fa970bf921396c09dec6eb0
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-18 23:24:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,68 @@
 class Dog
+  attr_accessor :name, :breed, :id
   
-  def attributes
-    attr_accessor :name, :breed, :id
-  end
-  
-  def initialize(name, breed, id = nil)
+  def initialize(name:, breed:, id: nil)
     @name = name
     @breed = breed 
     @id = id
   end
   
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        breed TEXT)
+    SQL
+    
+    DB[:conn].execute(sql)
   end
   
   def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
   end
   
   def save
-end
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+      
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+    self
+  end
 
-  def self.create
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
   end
   
   def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ? LIMIT 1"
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
+    sql = "SELECT * WHERE name = ? AND breed = ? LIMIT 1"
+    
+    dog = DB[:conn].execute(sql,name,breed)
+    
+    if dog 
+    
+    
   end 
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(name: name, breed: breed, id: id)
+  end
 end 
